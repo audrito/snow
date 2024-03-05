a82e6cb1b3cbe09c401d8adbec532040e1180520
Audrito <aleefchowdhury2@gmail.com>
2024-03-05 22:04:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import json
+import re
+import redis
 import discord
 import asyncio
 from langchain_community.llms import LlamaCpp
@@ -8,11 +11,15 @@ from ConversationTokenBufferMemory import ConversationTokenBufferMemory
 from langchain.chains import LLMChain
 from cleanput import fix_short_forms
 
+DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+
+redis_url = "redis://localhost:6379"
+
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+
 # Langchain Settings
-
-MODEL_PATH = "/content/snow/models/models--TheBloke--Thespis-Mistral-7B-v0.6-GGUF/snapshots/4f592294df9562c246e632dec8445d3965d84baa/thespis-mistral-7b-v0.6.Q8_0.gguf"
-
-llm = LlamaCpp(model_path=MODEL_PATH, n_gpu_layers=120, n_ctx=4096, n_batch=1024, max_tokens=-1, temperature=0.9, repeat_penalty=1.18, verbose=False)
+llm = LlamaCpp(model_path="E:\llama.cpp\synthia-7b-v2.0-16k.Q4_K_M.gguf", verbose=False, n_ctx=2048, n_batch=512, max_tokens=-1, temperature=0.9, repeat_penalty=1.18)
 
 # Discord Settings
 class MyClient(discord.Client):
@@ -24,8 +31,9 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
         asyncio.create_task(self.process_requests())
 
+
     async def on_message(self, message):
-        print(f'Message from {message.author.id}: {message.content}')
+        print(f'Message from {message.author}: {message.content}')
         if self.user != message.author:
             await self.request_queue.put(message)
 
@@ -39,16 +47,21 @@ class MyClient(discord.Client):
     async def process_request(self, message):
         if self.user != message.author:
             session_id = str(message.author.id)
-            history = RedisChatMessageHistory(session_id=session_id, url="redis://localhost:6379")
-            memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=3996, chat_memory=history)
+            history = RedisChatMessageHistory(session_id=session_id, url=redis_url)
+            memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=2048, chat_memory=history)
 
             if message.channel.type == discord.ChannelType.private or self.user in message.mentions:
-                system_message = "Act as a good, supportive and romantic girlfriend."
+                try:
+                    character_id = r.hget(f'user:{session_id}', 'selected_character')
+                    character_data = r.json().get(f'character:{character_id}')
+                    system_message = f"Act and roleplay as the following character.\n{character_data['data']['description']}"
+                except:
+                    system_message = "Act and roleplay as the following character."
 
                 template = '''Previous conversation:
-                {history}
-                Human: {question}
-                AI:'''
+{history}
+Human: {question}
+AI:'''
 
                 new_template = "\n".join([system_message, template])
 
@@ -56,16 +69,36 @@ class MyClient(discord.Client):
                 conversation = LLMChain(
                     llm=llm,
                     prompt=prompt,
-                    verbose=True,
+                    verbose=False,
                     memory=memory
                 )
                 # Show typing... indicator
                 async with message.channel.typing():
-                    response = await conversation.arun({"question": fix_short_forms(message.content)})
+                    response = await conversation.arun({"question": (message.content)})
                 channel = message.channel
                 await channel.send(response)
 
+    async def on_raw_reaction_add(self, payload):
+        channel = client.get_channel(payload.channel_id)
+        if channel is not None:
+            if isinstance(channel, discord.Thread) and channel.parent_id == 1214559209593511936:
+                # Get the message content
+                message = await channel.fetch_message(payload.message_id)
+                message_content = message.content
+                # Extract the last 4 characters from the message content
+                match = re.search(r'\[(\d{6})\]$', message_content)
+                if match:
+                    code = match.group(1)
+                    # Get the user id
+                    user_id = str(payload.user_id)
+
+                    # Update the hash for the user with the extracted 4 character code
+                    r.hset(f'user:{user_id}', 'selected_character', code)
+
 intents = discord.Intents.default()
+intents.reactions = True
+intents.members = True
+intents.messages = True
 intents.message_content = True
 
 client = MyClient(intents=intents)
